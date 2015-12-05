473f2759d6c3a8cf93fb6f8e17f52a15e4ada4ee
Drucatar <100303567@alumnos.uc3m.es>
2015-12-05 17:32:43 +0100

    ANDDDDDDDDDDDDDDDDDDDD

--- PersistenceJPAProject/src/entities/Course.java
+++ PersistenceJPAProject/src/entities/Course.java
@@ -15,8 +15,8 @@ import javax.persistence.*;
     @NamedQuery(name = "Course.findValidatedCourses" , query = "SELECT c FROM Course c WHERE c.validated = 1"),
     @NamedQuery(name = "Course.findByID" , query = "SELECT c FROM Course c WHERE c.idCourse = :courseID"),
     @NamedQuery(name = "Course.findByNickname", query = "SELECT c FROM Course c WHERE c.professor = :nickname"),
-    @NamedQuery(name = "Course.findByNicknameValidated", query = "SELECT c FROM Course c WHERE c.professor = :nickname c.validated = 1"),
-    @NamedQuery(name = "Course.findByNicknameNotValidated", query = "SELECT c FROM Course c WHERE c.professor = :nickname c.validated = 0"),
+    @NamedQuery(name = "Course.findByNicknameValidated", query = "SELECT c FROM Course c WHERE c.professor = :nickname AND c.validated = 1"),
+    @NamedQuery(name = "Course.findByNicknameNotValidated", query = "SELECT c FROM Course c WHERE c.professor = :nickname AND c.validated = 0"),
     @NamedQuery(name = "Course.findByTitle", query = "SELECT c FROM Course c WHERE c.title = :title"),
     @NamedQuery(name = "Course.findDescription", query = "SELECT c.descriptionCourse FROM Course c")
 })
